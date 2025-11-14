c67738cfc76591b2c2844657ab83370261d2fa61
Jan Buntenbroich <jan@buntenbroich.de>
2025-11-14 17:02:41 +0100

    p1 fertig

---
+++ src/p1/P1_main_1.java
@@ -0,0 +1,33 @@
+package p1;
+
+public class P1_main_1 {
+	public static void main(String[] args) {
+		int stockwerk = 1;
+		
+		switch (stockwerk) {
+		
+		case -1:
+			System.out.println("Parkhaus");
+			break;
+			
+		case 0:
+			System.out.println("Angebote");	
+			break;
+						
+		case 1:
+			System.out.println("Damenbekleidung");
+			break;
+						
+		case 2:
+			System.out.println("Herrenbekleidung");	
+			break;
+						
+		case 3:
+			System.out.println("Kinderbekleidung");
+			break;
+		
+		default:
+			System.out.println("unbekannt");
+		}
+	}
+}
